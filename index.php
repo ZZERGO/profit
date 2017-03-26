14648aba6f4f26acff8153ccc45b5fb2e6c49f33
ZZERGO <s.trushkov76@gmail.com>
2017-03-26 15:23:17 +0300

    + проверки при подключении автозагрузчика классов

--- index.php
+++ index.php
@@ -12,12 +12,31 @@ ini_set('display_errors', 1);
 // Задаём константы
 define('DS', DIRECTORY_SEPARATOR);
 define('ROOT', dirname(__FILE__));
+define('APP', ROOT . DS . 'App');
 define('TPL_DIR', ROOT . DS .'templates');
 
-//Подключаем автозагрузчик классов
-require_once ROOT . DS . 'app' . DS . 'core' . DS . 'autoloader.php';
-spl_autoload_register(['AutoLoad', 'loader']);
+
+// Подключаем автозагрузку и регистрируем в стеке
+try {
+    $file_loader = APP . DS. 'core' . DS . 'autoloader.php';
+    $class_loader = \App\Core\AutoLoad::class;
+    $method_loader = 'loader';
+
+    if (!is_readable($file_loader)){
+        throw new Exception('<h3>Не найден файл</h3>' . $file_loader );
+    }
+    require_once $file_loader;
+    if (!class_exists($class_loader)){
+        throw new Exception('<h3>Класс автозагрузчика не найден</h3>');
+    } elseif (!method_exists($class_loader, $method_loader)){
+        throw new Exception('<h3>Метод автозагрузчика не найден</h3>');
+    }
+    spl_autoload_register([$class_loader, $method_loader]);
+} catch (Exception $e) {
+    echo '<p><b>Ошибка в файле:</b><br>' . $e->getFile() . '</p>';
+    die($e->getMessage());
+}
 
 //Запускаем роутер
-\App\Core\Router::Run();
+//\App\Core\Router::Run();
 ?>
